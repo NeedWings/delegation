c459e67fb84e4918d65f037e4ee9049932c195be
NeedWings <136832174+NeedWings@users.noreply.github.com>
2024-02-24 12:36:23 +0300

    add task

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ try:
     from modules.utils.starter import *
     
 
-    if __name__ == "__main__":
-        pass
-        #checking_license()
 
     def get_action() -> str:
         theme = {
@@ -37,7 +34,7 @@ try:
                 "action",
                 message=colored("Choose soft work task", 'light_yellow'),
                 choices=[
-                    "claim"
+                    "delegator"
                 ],
             )
         ]
@@ -58,124 +55,15 @@ try:
         addresses = f.read().lower().split("\n")
         f.close()
         action = get_action()
-        if action == "claim":
+        if action == "delegator":
             task_number = 1
-        elif action == "Off bridge different wallet(EVM)":
-            task_number = 111
-        elif action == "Disperser":
-            task_number = 57
-        elif action == "Orbiter":
-            task_number = 120
-        elif action == "Orbiter different wallet(EVM)":
-            task_number = 121
-        elif action == "withdraw":
-            task_number = 130
-        elif action == "withdraw different wallet(EVM)":
-            task_number = 131
-        elif action == "random swaps":
-            task_number = 21
-        elif action == "swap to one token":
-            task_number = 22
-        elif action == "add liquidity":
-            task_number = 31
-        elif action == "remove liquidity":
-            task_number = 32
-        elif action == "lending task":
-            task_number = 41
-        elif action == "remove from lend":
-            task_number = 42
-        elif action == "return borrowed tokens":
-            task_number = 43
-        elif action == "collateral on zklend":
-            task_number = 44
-        elif action == "dmail":
-            task_number = 51
-        elif action == "starkstars":
-            task_number = 52
-        elif action == "starknet id":
-            task_number = 53
-        elif action == "bids on flex":
-            task_number = 541
-        elif action == "bids on unframed":
-            task_number = 542
-        elif action == "bids on element":
-            task_number = 543
-        elif action == "upgrade argent":
-            task_number = 551
-        elif action == "upgrade braavos":
-            task_number = 552
-        elif action == "deploy accounts":
-            task_number = 56
-        elif action == "Stress Test":
-            task_number = 58
-        elif action == "okx withdraw":
-            task_number = 61
-        elif action == "okx sender":
-            task_number = 62
-        elif action == "stats":
-            task_number = 71
-        elif action == "own tasks":
-            task_number = 0
-        elif action == "encrypt_secrets":
-            task_number = 11
-        elif action == "change private keys(argent only)":
-            task_number = 9997
-        elif action == "add public keys":
-            task_number = 9999
-        elif action == "show public keys":
-            task_number = 9998
-        elif action == "show public keys from keys from new_private_keys.txt":
-            task_number = 9995
-        elif action == "clear wallets_data":
-            task_number = 9994
-        elif action == "change private keys":
-            task_number = 9996
         for i in range(len(addresses)):
             if len(addresses[i]) < 50:
                 addresses[i] = "0x" + "0"*(42-len(addresses[i])) + addresses[i][2::]
             else:
                 addresses[i] = "0x" + "0"*(66-len(addresses[i])) + addresses[i][2::]
-        if task_number == 11:
-            encode_secrets()
-        elif task_number == 9999:
-            try:
-                with open(f"{SETTINGS_PATH}public_address_pairs.txt", "r") as f:
-                    new_pairs = f.read().split("\n")
-            except Exception as e:
-                input(f"Can't load public keys, most likely data not updated. Error: {e}")
-                exit()
-
-            for raw_pair in new_pairs:
-                if raw_pair == "":
-                    continue
-                pair = raw_pair.split(";")
-                PUBLIC_KEYS_PAIRS[normalize_to_32_bytes(pair[0])] = normalize_to_32_bytes(pair[1])
-
-            with open(f"{SETTINGS_PATH}wallets_data.ser", "wb") as f:
-                f.write(pickle.dumps(PUBLIC_KEYS_PAIRS))
-            input("All OK")
-            exit()
-        elif task_number == 9994:
-            with open(f"{SETTINGS_PATH}wallets_data.ser", "wb") as f:
-                f.write(pickle.dumps({}))
-            input("All OK")
-            exit()
-        elif task_number == 9998:
-            res = "Public key;address\n"
-            for i in PUBLIC_KEYS_PAIRS:
-                res += f"{i};{PUBLIC_KEYS_PAIRS[i]}\n"
-            with open(f"{SETTINGS_PATH}result.csv", "w") as f:
-                f.write(res)
-            input("check result.csv")
-            exit()
-        elif task_number == 9995:
-            res = "Public key;Private key\n"
-            for i in NEW_PRIVATE_KEYS:
-                res += f"{i};{NEW_PRIVATE_KEYS[i]}\n"
-            with open(f"{SETTINGS_PATH}result.csv", "w") as f:
-                f.write(res)
-            input("check result.csv")
-            exit()
+        if 0:
+            pass
         else:    
             private_keys = decode_secrets()
             accounts, counter = transform_keys(private_keys, addresses)
@@ -188,9 +76,6 @@ try:
 
             
             print(f"Bot found {counter} private keys to work")
-            if task_number == 9996:
-                console_log.error("\nWARNING\nPlease make sure you do everything right. If this is your first time running this, then check it on one account. \nA broken order of actions or an error in filling out can lead to the complete loss of the wallet. \nIf you're confident in what you're doing, press Enter")
-                input()
             if SETTINGS["useProxies"]:
                 loop = asyncio.new_event_loop()
 
